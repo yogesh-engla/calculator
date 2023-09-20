f4a7278685278b97b75a476875777d6845c3dcb9
yogesh-engla <141921435+yogesh-engla@users.noreply.github.com>
2023-09-20 18:59:03 +0530

    Add files via upload

---
+++ calculater.js
@@ -0,0 +1,25 @@
+let string = "";
+let buttons = document.querySelectorAll(".button");
+
+Array.from(buttons).forEach(function(button){
+    button.addEventListener( "click", function (e) {
+        if(e.target.innerHTML == "="){
+            string = eval(string);
+            document.querySelector("input").value = string;
+        }
+        else if(e.target.innerHTML == "AC"){
+            string = "";
+            document.querySelector("input").value = string;
+        }
+        else if(e.target.innerHTML == "DEL"){
+            string = string.substring(0,string.length-1);
+            document.querySelector("input").value = string;
+        }
+        else{
+            string = string + e.target.innerHTML;
+            document.querySelector("input").value = string;
+        }
+        console.log(e.target);
+        
+    })
+})
